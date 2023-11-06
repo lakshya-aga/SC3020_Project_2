28b4263f5b635308c1a4e3d4119ec69f1967bee9
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2023-11-07 00:30:50 +0800

    Query Input

---
+++ interface/src/components/QueryInput.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const SqlQueryInput = () => {
+  const [sqlQuery, setSqlQuery] = useState('');
+
+  const handleQueryChange = (event) => {
+    setSqlQuery(event.target.value);
+  };
+
+  const handleExecuteQuery = () => {
+    console.log('Executing SQL query:', sqlQuery);
+  };
+
+  return (
+    <div className="mb-3">
+      <textarea
+        className="form-control w-25"
+        rows="5"
+        placeholder="Enter your SQL query here..."
+        value={sqlQuery}
+        onChange={handleQueryChange}
+      ></textarea>
+      <button className="btn btn-primary mt-2" onClick={handleExecuteQuery}>
+        Execute Query
+      </button>
+    </div>
+  );
+};
+
+export default SqlQueryInput;
