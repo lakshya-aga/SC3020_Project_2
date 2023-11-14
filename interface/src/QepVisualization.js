5b765ab33ce375f51c5ba37ba4f7bf5eee5f49f5
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2023-11-14 15:44:15 +0800

    Tree

--- interface/src/QepVisualization.js
+++ interface/src/QepVisualization.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import Tree from 'react-d3-tree';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from 'react';
+import * as echarts from 'echarts';
 
-// This is a simplified example of an org chart with a depth of 2.
-// Note how deeper levels are defined recursively via the `children` property.
-const sample = {
-      "Node Type": "Aggregate",
+const OrgChart = ({ data }) => {
+  const sampleData = {
+    "Node Type": "Aggregate",
       
       "Plans": [
           {
@@ -104,68 +102,103 @@ const sample = {
               ]
           }
       ]
-  }
-  
+};
 
-
-
-function renameKeys(json) {
-  if (Array.isArray(json)) {
-    return json.map(item => renameKeys(item));
-  } else if (typeof json === 'object') {
-    const renamedObject = {};
-    for (const key in json) {
-      let newKey = key;
-      if (key === 'Plan' || key === 'Plans') {
-        newKey = 'children';
-      } else if (key === 'Node Type') {
-        newKey = 'name';
+  function renameKeys(json) {
+    if (Array.isArray(json)) {
+      return json.map(item => renameKeys(item));
+    } else if (typeof json === 'object') {
+      const renamedObject = {};
+      for (const key in json) {
+        let newKey = key;
+        if (key === 'Plan' || key === 'Plans') {
+          newKey = 'children';
+        } else if (key === 'Node Type') {
+          newKey = 'name';
+        }
+        renamedObject[newKey] = renameKeys(json[key]);
       }
-      renamedObject[newKey] = renameKeys(json[key]);
+      return renamedObject;
+    } else {
+      return json;
     }
-    return renamedObject;
-  } else {
-    return json;
   }
-}
-var dataChart = renameKeys(sample);
 
-export default function OrgChartTree() {
-  const [clickedNode, setClickedNode] = useState(null);
+  const dataChart = renameKeys(sampleData);
 
-  // Event handler for node click
-  const handleNodeClick = (nodeData, evt) => {
-    setClickedNode(nodeData);
-    console.log('Clicked Node:', nodeData);
-  };
+  useEffect(() => {
+    const chartDom = document.getElementById('orgChart');
+    const myChart = echarts.init(chartDom);
+    const option = {
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove',
+        formatter: (params) => {
+          const data = params.data;
+          return `Node Type: ${data.name}`;
+        },
+      },
+      series: [
+        {
+          type: 'tree',
+          id: 0,
+          name: 'tree1',
+          data: [dataChart],
+          top: '10%',
+          left: '8%',
+          bottom: '22%',
+          right: '20%',
+          symbol: 'rect',
+          symbolSize: 80,
+          orient: 'vertical',
+          edgeShape: 'polyline',
+          edgeForkPosition: '63%',
+          initialTreeDepth: -1,
+          itemStyle:{
+            color: '#5bc8ec'
+          },
+          lineStyle: {
+            width: 2,
+          },
+          label: {
+            position: 'inside',
+            verticalAlign: 'middle',
+            align: 'center',
+            width: 80,
+            fontSize: 16,
+            overflow:'break'
+          },
+          leaves: {
+            label: {
+              position: 'inside',
+              verticalAlign: 'middle',
+              width: 100,
+              align: 'center',
+              fontSize: 16,
+            },
+            itemStyle:{
+              color: '#0d566e'
+            },
+          },
+          emphasis: {
+            focus: 'descendant',
+          },
+          expandAndCollapse: false,
+          animationDuration: 550,
+          animationDurationUpdate: 750,
+        },
+      ],
+    };
+    option.series[0].data = [dataChart];
+    myChart.setOption(option);
 
-  // Event handler for link click
-  const handleLinkClick = (linkData, evt) => {
-    console.log('Clicked Link:', linkData);
-  };
+    // Clean up when component unmounts
+    return () => {
+      myChart.dispose();
+    };
+  }, [dataChart]);
 
-  return (
-    <div>
-      <div id="treeWrapper" style={{
-        alignItems: 'center',
-        width: '1000vw',
-        height: '1000vh',
-      }}>
-        <Tree 
-          data={dataChart}
-          orientation="vertical"
-          translate={{ x: 100, y: 100 }}
-          onClick={handleLinkClick} // Handle link clicks
-          onNodeClick={handleNodeClick} // Handle node clicks
-        />
-      </div>
-      <div>
-        {clickedNode && (
-          <pre>
-            Clicked Node Data:
-          </pre>
-        )}
-      </div>
-    </div>
-  );
-}
+  return <div id="orgChart" style={{ width: '100%', height: '100vh' }} />;
+};
+
+export default OrgChart;
